d1013ad7b75cdbeba93638f89a37372090e6e106
ngnhoangk147 <118551318+ngnhoangk147@users.noreply.github.com>
2022-11-19 22:24:52 +0700

    Update select_acc.sh

--- Script/select_acc.sh
+++ Script/select_acc.sh
@@ -2,15 +2,15 @@
 
 echo "Script generated."
 echo "Collecting data..."
-echo "set wrap off" > select_account.sql
-echo "set lines 155" >> select_account.sql
-echo "set pages 0" >> select_account.sql
-echo "set head off" >> select_account.sql
-echo "set feedback off" >> select_account.sql
-echo "spool accountinfo_today.csv" >> select_account.sql
-echo "select a.identify||','||b.name||','||a.ACCREDDT from ppm a inner join servclas b on a.servclas = b.ri where rownum<1000;" >> select_account.sql
-echo "spool off;" >> select_account.sql
-echo "exit" >> select_account.sql
+echo "set wrap off" > test_select_accountinfo.sql
+echo "set lines 155" >> test_select_accountinfo.sql
+echo "set pages 0" >> test_select_accountinfo.sql
+echo "set head off" >> test_select_accountinfo.sql
+echo "set feedback off" >> test_select_accountinfo.sql
+echo "spool accountinfo_today.csv" >> test_select_accountinfo.sql
+echo "a.identify||','||b.name||','||c.USCREDVO from ppm a inner join servclas b on a.servclas = b.ri inner join credit c on a.ri=c.ri where rownum<1000;" >> test_select_accountinfo.sql
+echo "spool off;" >> test_select_accountinfo.sql
+echo "exit" >> test_select_accountinfo.sql
 export ORACLE_SID=PSMF
 sqlplus user/pass @test_select_accountinfo.sql
 cat accountinfo_today.csv >> accountinfo.csv
